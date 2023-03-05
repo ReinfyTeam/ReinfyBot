198c7ec7206c344522eba4055fde32bd5536c6e6
Ace Sobremont <xqwtxon@hotmail.com>
2023-03-05 12:56:26 +0800

    Lint test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, Intents, Collection } = require("discord.js");
 const fs = require("fs");
-
+ 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
